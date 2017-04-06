263be7ffe99a429b946011a97c2367afcf0d8ee0
jafernandez <jafernandez@lug044.zylk.net>
2017-04-06 17:04:29 +0200

    Package renamed.

--- src/main/java/com/bigdata/flinkconsumer/FlinkConsumer.java
+++ src/main/java/com/bigdata/flinkconsumer/FlinkConsumer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.bilbomatica.flinkconsumer;
+package com.bigdata.flinkconsumer;
 
 import java.io.IOException;
 import java.util.Date;
